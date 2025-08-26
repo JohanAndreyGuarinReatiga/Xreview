826c2185ec7452e6ee3d871493ac842435ecb7b9
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2025-08-26 11:49:38 -0500

    feat: :sparkles: servicio de resenias, creacion

--- services/reseniaServ.js
+++ services/reseniaServ.js
@@ -1,3 +1,38 @@
-import { getDB } from "../config/configdb";
-import {Resenia} from "../models/resenia";
+import { getDB } from "../config/configdb.js";
+import { Resenia } from "../models/Resenia.js";
+import { ObjectId } from "mongodb";
 
+export class ServicioResenias {
+  static async crearResenia(data) {
+    const session = getDB().client.startSession();
+
+    try {
+      let nuevaResenia = new Resenia(data);
+
+      let result;
+      await session.withTransaction(async () => {
+        result = await getDB()
+          .collection("resenias")
+          .insertOne(nuevaResenia.toDBObject(), { session });
+
+        // recalcular promedio
+        const stats = await getDB().collection("resenias").aggregate(
+          [
+            { $match: { tituloId: nuevaResenia.tituloId } },
+            { $group: { _id: null, avgCalif: { $avg: "$calificacion" } } },
+          ],
+          { session }
+        ).toArray();
+
+        await getDB().collection("titulos").updateOne(
+          { _id: nuevaResenia.tituloId },
+          { $set: { promedioCalificacion: stats[0]?.avgCalif || nuevaResenia.calificacion } },
+          { session }
+        );
+      });
+
+      return { ...nuevaResenia.toDBObject(), insertedId: result.insertedId };
+    } finally {
+      await session.endSession();
+    } }
+}
